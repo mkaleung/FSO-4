6c1b9d62c7d0e96e46627e981615a4a7e51f1f1b
jincrypt <leung.mka@gmail.com>
2023-03-13 22:59:06 -0400

    new function and test block for favorite blog

--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -8,6 +8,21 @@ const totalLikes = (blogs) => {
   return sumLikes
 }
 
+const favoriteBlog = (blogs) => {
+  let favorite = 0
+  let maxLikes = 0
+
+  for (i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > maxLikes) {
+      maxLikes = blogs[i].likes;
+      favorite = i
+    }
+  }
+
+  return blogs[favorite]
+}
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
