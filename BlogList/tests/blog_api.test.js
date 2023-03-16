80a59dff32a5cc5cf367115d41a42be78746a6fe
jincrypt <leung.mka@gmail.com>
2023-03-16 11:57:19 -0400

    added initialization and fixed url based on NODE_ENV

--- BlogList/tests/blog_api.test.js
+++ BlogList/tests/blog_api.test.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
 
 test('blogs are returned as json', async () => {
   await api
