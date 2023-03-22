044b75f520331c0a672fd5fef515e98a4ea3d24e
jincrypt <leung.mka@gmail.com>
2023-03-22 17:06:36 -0400

    Added user creation validation and test cases

--- BlogList/tests/blog_api.test.js
+++ BlogList/tests/blog_api.test.js
@@ -3,7 +3,9 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -118,6 +120,95 @@ test('confirm likes can be updated with HTTP put', async () => {
 
   expect(blogsAtEnd[0].likes).toBe(updatedBlog.likes)
 })
+
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('supersecret', 10)
+    const user = new User({ username: 'root', name:'Test User', passwordHash })
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'initialUser',
+      name: 'Test User',
+      password: 'secret',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Test User',
+      password: 'supersecret',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+
+  test('creation fails if username and/or password is less than 3 in length', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const userWithInvalidUsername = {
+      username: 'ro',
+      name: 'Test User',
+      password: 'supersecret',
+    }
+    
+    const userWithInvalidPassword = {
+      username: 'roost',
+      name: 'Test User',
+      password: 't',
+    }
+
+    const resultInvalidUsername = await api
+      .post('/api/users')
+      .send(userWithInvalidUsername)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(resultInvalidUsername.body.error).toContain('shorter than the minimum allowed length (3).')
+
+    const resultInvalidPassword = await api
+      .post('/api/users')
+      .send(userWithInvalidPassword)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(resultInvalidPassword.body.error).toContain('Password must be at least 3 characters long')
+    
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+})
+
 
 afterAll(async () => {
   await mongoose.connection.close()
