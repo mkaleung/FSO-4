31b24d654148c25d615aa9d5282eda57e5274d03
jincrypt <leung.mka@gmail.com>
2023-03-22 22:23:49 -0400

    Updated delete function to only be possible if token matches blog post creator

--- BlogList/controllers/blogs.js
+++ BlogList/controllers/blogs.js
@@ -34,8 +34,20 @@ blogRouter.post('/', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()  
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const targetBlog = await Blog.findById(request.params.id)
+
+  if (targetBlog.user.toString() === user.id.toString() ) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    response.status(401).json({ error: 'incorrect user'})
+  }
 })
 
 blogRouter.put('/:id', async (request, response) => {
